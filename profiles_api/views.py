3181c20645bd25b31b41f17c6a69cc04f7d7e9cc
Sean <pereirasean1811@gmail.com>
2021-01-31 19:01:04 -0800

    add search parameter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from profiles_api import serializers
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 
+from rest_framework import filters
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -106,8 +108,10 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
 
-    
+
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)

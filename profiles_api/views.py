6fd232a9b1708d79bf22e3b5c3cfbff8423064d9
Sean <pereirasean1811@gmail.com>
2021-01-31 18:56:06 -0800

    added permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-
+from profiles_api import models
 from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -55,7 +57,7 @@ class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
     serializer_class = serializers.HelloSerializer
-    
+
     def list(self, request):
         """Return a hello message."""
 
@@ -100,3 +102,12 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+
+    
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
